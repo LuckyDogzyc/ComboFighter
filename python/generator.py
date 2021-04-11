3b4709a594973376d37ef3415a82cf7f47866be0
LuckyDogzyc <zhaoyinchu1998@163.com>
2021-04-11 14:57:48 -0400

    Add files via upload

--- python/generator.py
+++ python/generator.py
@@ -6,7 +6,7 @@ def generateRandChild(): #init the random commands to control AI
 	for i in range(popSize): #popSize times - 10
 		actionList = ""
 		for y in range(seqLen): #seqLen times - 1000
-			actionList += (commands[random.randint(0,6)])
+			actionList += (commands[random.randint(0,7)])
 		commandList.append(actionList)
 
 	# writing to file
@@ -25,7 +25,7 @@ parentRand = 0.5  # chance to select action from parent 1 (50/50)
 mutRand = 0.3  # chance to mutate offspring action
 
 bestSeq = []  # best sequence to use in case iterations max out
-commands = ("A","B","C","U","D","L","R")
+commands = ("A","B","C","U","D","L","R","N")
 
 commandList = []
 
